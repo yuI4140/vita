e8b7c9d4c1b7174b8ca2f41438a4b1b93770f8eb
yuI4140 <greyberzambrano79@gmail.com>
2023-05-21 12:35:59 +0200

    update 1.0

--- vita.h
+++ vita.h
@@ -3,32 +3,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#define VCOLOR_DEFAULT 0
+#define VCOLOR_BLACK 1
+#define VCOLOR_RED 2
+#define VCOLOR_GREEN 3
+#define VCOLOR_YELLOW 4
+#define VCOLOR_BLUE 5
+#define VCOLOR_MAGENTA 6
+#define VCOLOR_CYAN 7
+#define VCOLOR_WHITE 8
 typedef struct {
   int x, y;          // Window position
   int width, height; // Window size
   const char *title; // Window title
 } Window;
-
+typedef struct {
+  int x, y;
+} Cursor;
+// Init Window
 void vInit();
+// exit Window
 void vExit();
+// render window
 void vRWin(const Window *window);
 bool isKey(int key);
 void vClear();
 int vGetKey();
+// move cursor from Cursor struct
+void vCrsrMov(Cursor *crsr);
 void vWaitForKey();
 void vFlushInput();
 void vPrintChar(char ch);
-void vPrintString(const char *str);
-void vPrintFormattedString(const char *format, ...);
-void vMoveCursor(int x, int y);
-int vGetCursorX();
-int vGetCursorY();
-int vGetWindowWidth();
-int vGetWindowHeight();
+void vPrintStr(const char *str);
+// vPrintStr with format
+void vPrintF(const char *format, ...);
+// move cursor from x and y
+void vMovCrsr(int x, int y);
+// get Cursor-X
+int vGetCrsrX();
+// get Cursor-Y
+int vGetCrsrY();
+// get WIndow Width
+int vGetWinW();
+// get Window Height
+int vGetWinH();
+// Set text attributes
+void vSetAttr(int foregroundColor, int backgroundColor);
 
 #ifdef V_IMP
-
 void vClear() { werase(stdscr); }
 
 void vInit() {
@@ -36,6 +58,7 @@ void vInit() {
   cbreak();             // Disable line buffering
   noecho();             // Don't echo user input
   keypad(stdscr, true); // Enable function keys
+  start_color();        // Enable color support
 }
 
 void vExit() {
@@ -72,31 +95,37 @@ void vFlushInput() { flushinp(); }
 
 void vPrintChar(char ch) { addch(ch); }
 
-void vPrintString(const char *str) { addstr(str); }
+void vPrintStr(const char *str) { addstr(str); }
 
-void vPrintFormattedString(const char *format, ...) {
+void vPrintF(const char *format, ...) {
   va_list args;
   va_start(args, format);
   vwprintw(stdscr, format, args);
   va_end(args);
 }
 
-void vMoveCursor(int x, int y) { move(y, x); }
+void vMovCrsr(int x, int y) { move(y, x); }
 
-int vGetCursorX() {
+void vCrsrMov(Cursor *crsr) { move(crsr->y, crsr->x); }
+
+int vGetCrsrX() {
   int x, y;
   getyx(stdscr, y, x);
   return x;
 }
 
-int vGetCursorY() {
+int vGetCrsrY() {
   int x, y;
   getyx(stdscr, y, x);
   return y;
 }
 
-int vGetWindowWidth() { return COLS; }
+int vGetWinW() { return COLS; }
 
-int vGetWindowHeight() { return LINES; }
+int vGetWinH() { return LINES; }
+
+void vSetAttr(int foregroundColor, int backgroundColor) {
+  attron(COLOR_PAIR(foregroundColor) | (backgroundColor << 3));
+}
 
 #endif
